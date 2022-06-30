ad126f555fa48b73b4c76f8127b0a56c87b276f3
Muhamad Fikri Nurohman <93314435+fikfikriii@users.noreply.github.com>
2022-06-30 17:08:18 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -9,3 +9,22 @@ const pool = new Pool({
     // },
 });
 module.exports = pool;
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+});
+
+client.connect();
+
+client.query('SELECT * FROM tws;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
